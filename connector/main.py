b2332cd33be441f29c9a587a2efefe5c7151ea05
Marco <marco.tollini1@swisscom.com>
2021-01-05 10:59:54 +0100

    removed code useless

---
+++ connector/main.py
@@ -0,0 +1,17 @@
+from confluent_kafka import Consumer
+
+
+def main():
+    consumer = Consumer({
+        'bootstrap.servers': conf['bootstrap.servers'],
+        'sasl.mechanisms': conf['sasl.mechanisms'],
+        'security.protocol': conf['security.protocol'],
+        'sasl.username': conf['sasl.username'],
+        'sasl.password': conf['sasl.password'],
+        'group.id': 'python_example_group_1',
+        'auto.offset.reset': 'earliest',
+    })
+
+
+if __name__ == '__main__':
+    main()
