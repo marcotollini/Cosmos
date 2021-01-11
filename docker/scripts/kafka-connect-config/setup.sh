332cf112664cab2e88a589880c3241fadd003ccc
Marco <marco.tollini1@swisscom.com>
2021-01-11 14:34:10 +0100

    conversion to timescaledb

--- docker/scripts/kafka-connect-config/setup.sh
+++ docker/scripts/kafka-connect-config/setup.sh
@@ -6,18 +6,20 @@ curl -X POST \
   "config": {
     "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
     "tasks.max": 1,
-    "connection.url": "jdbc:postgresql://postgres:5432/l3visualization?user=l3visualization&password=postgres_password",
+    "connection.url": "jdbc:postgresql://timescale:5432/l3visualization?user=l3visualization&password=postgres_password",
     "topics": "nfacctd_bmp",
     "table.name.format": "event_init",
-    "transforms": "filtermsg,timestamp0000,casttimestamp",
+    "transforms": "filtermsg,timestamp0000,castfloattimestamp,castinttimestamp",
     "transforms.filtermsg.type": "io.confluent.connect.transforms.Filter$Value",
     "transforms.filtermsg.filter.condition": "$[?(@.event_type == \"log\" && @.bmp_msg_type == \"init\")]",
     "transforms.filtermsg.filter.type": "include",
     "transforms.timestamp0000.type": "com.github.marcotollini.kafka.connect.nfacctd.smt.RegexNullify$Value",
     "transforms.timestamp0000.fields": "timestamp,timestamp_event,timestamp_arrival",
     "transforms.timestamp0000.regex": "0.000000",
-    "transforms.casttimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
-    "transforms.casttimestamp.spec": "timestamp:float64,timestamp_event:float64,timestamp_arrival:float64"
+    "transforms.castfloattimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
+    "transforms.castfloattimestamp.spec": "timestamp:float64,timestamp_event:float64,timestamp_arrival:float64,
+    "transforms.castinttimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
+    "transforms.castinttimestamp.spec": "timestamp:int32,timestamp_event:int32,timestamp_arrival:int32"
     }
   }' \
   http://kafka-connect:28083/connectors
@@ -29,18 +31,20 @@ curl -X POST \
   "config": {
     "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
     "tasks.max": 1,
-    "connection.url": "jdbc:postgresql://postgres:5432/l3visualization?user=l3visualization&password=postgres_password",
+    "connection.url": "jdbc:postgresql://timescale:5432/l3visualization?user=l3visualization&password=postgres_password",
     "topics": "nfacctd_bmp",
     "table.name.format": "event_log_init",
-    "transforms": "filtermsg,timestamp0000,casttimestamp",
+    "transforms": "filtermsg,timestamp0000,castfloattimestamp,castinttimestamp",
     "transforms.filtermsg.type": "io.confluent.connect.transforms.Filter$Value",
     "transforms.filtermsg.filter.condition": "$[?(@.event_type == \"log_init\")]",
     "transforms.filtermsg.filter.type": "include",
     "transforms.timestamp0000.type": "com.github.marcotollini.kafka.connect.nfacctd.smt.RegexNullify$Value",
     "transforms.timestamp0000.fields": "timestamp",
     "transforms.timestamp0000.regex": "0.000000",
-    "transforms.casttimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
-    "transforms.casttimestamp.spec": "timestamp:float64"
+    "transforms.castfloattimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
+    "transforms.castfloattimestamp.spec": "timestamp:float64",
+    "transforms.castinttimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
+    "transforms.castinttimestamp.spec": "timestamp:int32"
     }
   }' \
   http://kafka-connect:28083/connectors
@@ -51,18 +55,20 @@ curl -X POST \
   "config": {
     "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
     "tasks.max": 1,
-    "connection.url": "jdbc:postgresql://postgres:5432/l3visualization?user=l3visualization&password=postgres_password",
+    "connection.url": "jdbc:postgresql://timescale:5432/l3visualization?user=l3visualization&password=postgres_password",
     "topics": "nfacctd_bmp",
     "table.name.format": "event_peer_down",
-    "transforms": "filtermsg,timestamp0000,casttimestamp",
+    "transforms": "filtermsg,timestamp0000,castfloattimestamp,castinttimestamp",
     "transforms.filtermsg.type": "io.confluent.connect.transforms.Filter$Value",
     "transforms.filtermsg.filter.condition": "$[?(@.event_type == \"log\" && @.bmp_msg_type == \"peer_down\")]",
     "transforms.filtermsg.filter.type": "include",
     "transforms.timestamp0000.type": "com.github.marcotollini.kafka.connect.nfacctd.smt.RegexNullify$Value",
     "transforms.timestamp0000.fields": "timestamp,timestamp_event,timestamp_arrival",
     "transforms.timestamp0000.regex": "0.000000",
-    "transforms.casttimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
-    "transforms.casttimestamp.spec": "timestamp:float64,timestamp_event:float64,timestamp_arrival:float64"
+    "transforms.castfloattimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
+    "transforms.castfloattimestamp.spec": "timestamp:float64,timestamp_event:float64,timestamp_arrival:float64,
+    "transforms.castinttimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
+    "transforms.castinttimestamp.spec": "timestamp:int32,timestamp_event:int32,timestamp_arrival:int32"
     }
   }' \
   http://kafka-connect:28083/connectors
@@ -73,10 +79,10 @@ curl -X POST \
   "config": {
     "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
     "tasks.max": 1,
-    "connection.url": "jdbc:postgresql://postgres:5432/l3visualization?user=l3visualization&password=postgres_password",
+    "connection.url": "jdbc:postgresql://timescale:5432/l3visualization?user=l3visualization&password=postgres_password",
     "topics": "nfacctd_bmp",
     "table.name.format": "event_peer_up",
-    "transforms": "filtermsg,timestamp0000,castbool,casttimestamp",
+    "transforms": "filtermsg,timestamp0000,castbool,castfloattimestamp,castinttimestamp",
     "transforms.filtermsg.type": "io.confluent.connect.transforms.Filter$Value",
     "transforms.filtermsg.filter.condition": "$[?(@.event_type == \"log\" && @.bmp_msg_type == \"peer_up\")]",
     "transforms.filtermsg.filter.type": "include",
@@ -85,8 +91,10 @@ curl -X POST \
     "transforms.timestamp0000.regex": "0.000000",
     "transforms.castbool.type": "org.apache.kafka.connect.transforms.Cast$Value",
     "transforms.castbool.spec": "is_in:boolean,is_filtered:boolean,is_loc:boolean,is_post:boolean,is_out:boolean",
-    "transforms.casttimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
-    "transforms.casttimestamp.spec": "timestamp:float64,timestamp_event:float64,timestamp_arrival:float64"
+    "transforms.castfloattimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
+    "transforms.castfloattimestamp.spec": "timestamp:float64,timestamp_event:float64,timestamp_arrival:float64,
+    "transforms.castinttimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
+    "transforms.castinttimestamp.spec": "timestamp:int32,timestamp_event:int32,timestamp_arrival:int32"
     }
   }' \
   http://kafka-connect:28083/connectors
@@ -94,14 +102,14 @@ curl -X POST \
 
 curl -X POST \
   -H "Content-Type: application/json" \
-  --data '{ "name": "jdbc-route_monitor",
+  --data '{ "name": "jdbc-route_monitor2",
   "config": {
     "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
     "tasks.max": 1,
-    "connection.url": "jdbc:postgresql://postgres:5432/l3visualization?user=l3visualization&password=postgres_password",
+    "connection.url": "jdbc:postgresql://timescale:5432/l3visualization?user=l3visualization&password=postgres_password",
     "topics": "nfacctd_bmp",
     "table.name.format": "event_route_monitor",
-    "transforms": "filtermsg,timestamp0000,castbool,tojson,casttimestamp",
+    "transforms": "filtermsg,timestamp0000,castbool,tojson,castfloattimestamp,castinttimestamp",
     "transforms.filtermsg.type": "io.confluent.connect.transforms.Filter$Value",
     "transforms.filtermsg.filter.condition": "$[?(@.event_type == \"log\" && @.bmp_msg_type == \"route_monitor\")]",
     "transforms.filtermsg.filter.type": "include",
@@ -113,8 +121,10 @@ curl -X POST \
     "transforms.tojson.type": "com.github.marcotollini.kafka.connect.nfacctd.smt.ToJsonList$Value",
     "transforms.tojson.fields": "comms,ecomms,lcomms,as_path",
     "transforms.tojson.separator": "\\space",
-    "transforms.casttimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
-    "transforms.casttimestamp.spec": "timestamp:float64,timestamp_arrival:float64"
+    "transforms.castfloattimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
+    "transforms.castfloattimestamp.spec": "timestamp:float64,timestamp_arrival:float64",
+    "transforms.castinttimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
+    "transforms.castinttimestamp.spec": "timestamp:int32,timestamp_arrival:int32"
     }
   }' \
   http://kafka-connect:28083/connectors
@@ -126,10 +136,10 @@ curl -X POST \
   "config": {
     "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
     "tasks.max": 1,
-    "connection.url": "jdbc:postgresql://postgres:5432/l3visualization?user=l3visualization&password=postgres_password",
+    "connection.url": "jdbc:postgresql://timescale:5432/l3visualization?user=l3visualization&password=postgres_password",
     "topics": "nfacctd_bmp",
     "table.name.format": "event_stats",
-    "transforms": "filtermsg,timestamp0000,castbool,casttimestamp",
+    "transforms": "filtermsg,timestamp0000,castbool,castfloattimestamp,castinttimestamp",
     "transforms.filtermsg.type": "io.confluent.connect.transforms.Filter$Value",
     "transforms.filtermsg.filter.condition": "$[?(@.event_type == \"log\" && @.bmp_msg_type == \"stats\")]",
     "transforms.filtermsg.filter.type": "include",
@@ -138,8 +148,10 @@ curl -X POST \
     "transforms.timestamp0000.regex": "0.000000",
     "transforms.castbool.type": "org.apache.kafka.connect.transforms.Cast$Value",
     "transforms.castbool.spec": "is_in:boolean,is_filtered:boolean,is_loc:boolean,is_post:boolean,is_out:boolean",
-    "transforms.casttimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
-    "transforms.casttimestamp.spec": "timestamp:float64,timestamp_arrival:float64"
+    "transforms.castfloattimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
+    "transforms.castfloattimestamp.spec": "timestamp:float64,timestamp_arrival:float64",
+    "transforms.castinttimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
+    "transforms.castinttimestamp.spec": "timestamp:int32,timestamp_arrival:int32"
     }
   }' \
   http://kafka-connect:28083/connectors
@@ -151,18 +163,20 @@ curl -X POST \
   "config": {
     "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
     "tasks.max": 1,
-    "connection.url": "jdbc:postgresql://postgres:5432/l3visualization?user=l3visualization&password=postgres_password",
+    "connection.url": "jdbc:postgresql://timescale:5432/l3visualization?user=l3visualization&password=postgres_password",
     "topics": "nfacctd_bmp",
     "table.name.format": "event_log_close",
-    "transforms": "filtermsg,timestamp0000,casttimestamp",
+    "transforms": "filtermsg,timestamp0000,castfloattimestamp,castinttimestamp",
     "transforms.filtermsg.type": "io.confluent.connect.transforms.Filter$Value",
     "transforms.filtermsg.filter.condition": "$[?(@.event_type == \"log_close\")]",
     "transforms.filtermsg.filter.type": "include",
     "transforms.timestamp0000.type": "com.github.marcotollini.kafka.connect.nfacctd.smt.RegexNullify$Value",
     "transforms.timestamp0000.fields": "timestamp",
     "transforms.timestamp0000.regex": "0.000000",
-    "transforms.casttimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
-    "transforms.casttimestamp.spec": "timestamp:float64"
+    "transforms.castfloattimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
+    "transforms.castfloattimestamp.spec": "timestamp:float64",
+    "transforms.castinttimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
+    "transforms.castinttimestamp.spec": "timestamp:int32"
     }
   }' \
   http://kafka-connect:28083/connectors
@@ -173,18 +187,20 @@ curl -X POST \
   "config": {
     "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
     "tasks.max": 1,
-    "connection.url": "jdbc:postgresql://postgres:5432/l3visualization?user=l3visualization&password=postgres_password",
+    "connection.url": "jdbc:postgresql://timescale:5432/l3visualization?user=l3visualization&password=postgres_password",
     "topics": "nfacctd_bmp",
     "table.name.format": "event_term",
-    "transforms": "filtermsg,timestamp0000,casttimestamp",
+    "transforms": "filtermsg,timestamp0000,castfloattimestamp,castinttimestamp",
     "transforms.filtermsg.type": "io.confluent.connect.transforms.Filter$Value",
     "transforms.filtermsg.filter.condition": "$[?(@.event_type == \"log\" && @.bmp_msg_type == \"term\")]",
     "transforms.filtermsg.filter.type": "include",
     "transforms.timestamp0000.type": "com.github.marcotollini.kafka.connect.nfacctd.smt.RegexNullify$Value",
     "transforms.timestamp0000.fields": "timestamp,timestamp_event,timestamp_arrival",
     "transforms.timestamp0000.regex": "0.000000",
-    "transforms.casttimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
-    "transforms.casttimestamp.spec": "timestamp:float64,timestamp_event:float64,timestamp_arrival:float64"
+    "transforms.castfloattimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
+    "transforms.castfloattimestamp.spec": "timestamp:float64,timestamp_event:float64,timestamp_arrival:float64,
+    "transforms.castinttimestamp.type": "org.apache.kafka.connect.transforms.Cast$Value",
+    "transforms.castinttimestamp.spec": "timestamp:int32,timestamp_event:int32,timestamp_arrival:int32"
     }
   }' \
   http://kafka-connect:28083/connectors
