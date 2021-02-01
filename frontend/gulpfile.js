0c70f91ccd97a280098d3dabf25709672f75d51c
Marco <marco.tollini1@swisscom.com>
2021-02-01 21:20:36 +0100

    setup change

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -7,7 +7,6 @@ const fancy_log = require('fancy-log');
 const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
 const buffer = require('vinyl-buffer');
-const browserSync = require('browser-sync');
 
 const paths = {
   pages: ['src/*.html'],
@@ -23,20 +22,6 @@ const watchedBrowserify = watchify(
   }).plugin(tsify)
 );
 
-const server = browserSync.create();
-
-function reload() {
-  server.reload();
-}
-
-function serve() {
-  server.init({
-    server: {
-      baseDir: './dist',
-    },
-  });
-}
-
 gulp.task('copy-html', () => {
   return gulp.src(paths.pages).pipe(gulp.dest('dist'));
 });
@@ -48,14 +33,12 @@ function bundle() {
     .pipe(source('bundle.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(uglify())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist'));
 }
 
-gulp.task('default', gulp.series(gulp.parallel('copy-html'), bundle, serve));
+gulp.task('default', gulp.series(gulp.parallel('copy-html'), bundle));
 watchedBrowserify.on('update', () => {
   bundle();
-  reload();
 });
 watchedBrowserify.on('log', fancy_log);
