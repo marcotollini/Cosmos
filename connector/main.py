4607a1e6ce97a3ba9759ac08517bda3c0f0b7122
Marco <marco.tollini1@swisscom.com>
2021-03-01 15:37:09 +0100

    init connector + json deployment

---
+++ connector/main.py
@@ -0,0 +1,8 @@
+# https://hakibenita.com/fast-load-data-python-postgresql
+
+def main():
+    pass
+    # message = downloader()
+    # message_decoded = decoder(message)
+    # transformed_message = transformer(message_decoded)
+    # uploader(transformed_message)
